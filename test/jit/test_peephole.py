dde161ca9bf79451de6b682457f41bdecc14bb98
zhxchen17 <zhxchen17@fb.com>
2021-07-16 01:11:22 -0700

    [jit] Arithmetic simplification for integers.
    Summary:
    Add a mini pass to merge arithmetic nodes like (((x - 1) + 2) * 1) - 1.
    Issue #60913
    
    Test Plan:
    python test/test_jit.py TestPeephole.test_peephole_arith
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 2d667fbde0b32affe06fb3492ba2efe03ac5f3ba
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61444

--- test/jit/test_peephole.py
+++ test/jit/test_peephole.py
@@ -554,3 +554,21 @@ class TestPeephole(JitTestCase):
         self.run_pass("peephole", foo.graph)
         FileCheck().check("aten::len").run(foo.graph)
         self.assertEqual(3, foo(torch.rand([3, 1])))
+
+    def test_peephole_arith(self):
+        @torch.jit.script
+        def foo(input: List[int]):
+            _1 = torch.add(input[1], 2)
+            _3 = torch.add(input[3], 2)
+            _5 = torch.add(1, torch.sub(_1, 3) // 1)
+            _6 = torch.add(1 * torch.sub(_3, 3) // 1, 1) / 1
+            return [_5, int(_6)]
+
+        FileCheck().check("aten::add").check("aten::sub") \
+                   .check("aten::mul").check("aten::floordiv") \
+                   .check("aten::div").run(foo.graph)
+        self.run_pass("peephole", foo.graph)
+        FileCheck().check_not("aten::add").check_not("aten::sub") \
+                   .check_not("aten::mul").check_not("aten::floordiv") \
+                   .check_not("aten::div").run(foo.graph)
+        self.assertEqual(foo([0, 1, 2, 3]), [1, 3])
